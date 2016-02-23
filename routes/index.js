369561e0c402d86d3d5639e440ce552e62fb1404
ParvatiPatel <paro.patel@rocketmail.com>
2016-02-22 22:36:59 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+/* GET about page. */
+router.get('/aboutus', function(req, res, next) {
+  res.render('aboutus', { title: 'About Us' });
+});
+
+module.exports = router;
